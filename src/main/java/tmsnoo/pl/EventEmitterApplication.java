a86a864e0fe5478e5ca3d325ef3d4964e6ac0869
Tmsnoo <tomek.laskawiec@gmail.com>
2018-07-11 19:51:22 +0200

    second

---
+++ src/main/java/tmsnoo/pl/EventEmitterApplication.java
@@ -0,0 +1,12 @@
+package tmsnoo.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEmitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEmitterApplication.class, args);
+	}
+}
